ddd18ae135d8e41e952e75ea5abff7e94d5ee445
chenxi <chenxi@tigerjoys.com>
2016-09-05 19:21:44 +0800

    about devise.

--- products/config/routes.rb
+++ products/config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :products
   mount ApplicationAPI, at: 'api'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'products#index'
-
+  root 'products#index'
+  devise_for :users
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
