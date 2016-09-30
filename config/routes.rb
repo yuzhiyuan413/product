9307e84eb95b1566b6477dae7071a92ca3223d22
yuzhiyuan413 <1048921160@qq.com>
2016-09-30 14:32:33 +0800

    添加orders

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'products#index'
   devise_for :users
+  resources :users do 
+    resources :orders
+  end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
