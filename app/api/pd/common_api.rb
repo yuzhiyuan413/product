eea68d877794eea68238816ba0d9dd4bd351a496
chenxi <chenxi@tigerjoys.com>
2016-10-11 15:54:33 +0800

    product restful api.

--- app/api/pd/common_api.rb
+++ app/api/pd/common_api.rb
@@ -1,19 +1,53 @@
 module Pd
   class CommonAPI < ApplicationAPI
     resource :products do
-			get do
+      desc "Return all product."
+	  get do
         Product.all
-			end
-
+	  end
+	  
+	  desc "Return a product."	
       params do
         requires :id, type: Integer, desc: ' id.'
       end
       route_param :id do
         get do
-          guard!  #独立guard
+          guard!
           Product.find(params[:id])
         end
-      end     
+      end
+	  
+	  desc "Create a product."	
+      post do
+      	guard!
+      	params.delete("access_token")
+      	Product.create!(params.to_h) ? {msg: 'ok'} : {msg: 'fail'}
+      end
+	  
+	  desc "Update a product."	
+      params do
+      	requires :id, type: Integer, desc: ' id.'
+      	requires :name, type: String, desc: ' name.'
+      	requires :sprice, type: Float, desc: ' sprice.'
+      	requires :desc, type: String, desc: ' desc.'
+      end
+      put ':id' do
+      	guard!
+      	params.delete("access_token")
+		product = Product.find(params[:id])
+		product.update(params.to_h) ? {msg: 'ok'} : {msg: 'fail'}
+      end
+
+      desc "Delete a product."
+      params do
+        requires :id, type: Integer, desc: " id."
+      end
+      delete ':id' do
+      	guard!
+       	product = Product.find(params[:id])
+       	product.destroy ? {msg: 'ok'} : {msg: 'fail'}
+      end
+		
   	end
   end
 
