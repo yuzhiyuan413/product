41fa6f9de30732abeb61242dcedd75383625982a
chenxi <chenxi@tigerjoys.com>
2016-11-03 14:36:39 +0800

    注册和忘记密码界面，统一body模板

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def include_body_css
+    %W{
+    	users/sessions 
+    	users/registrations 
+    	devise/passwords
+    }.include?(params[:controller]) ? 
+    "loginBackground" : ""
+	end
 end
