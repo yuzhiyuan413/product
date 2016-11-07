2e848f424b88c14ec87406bfa667fcdfbf5cf666
chenxi <chenxi@tigerjoys.com>
2016-11-07 14:37:56 +0800

    fixed conflicts.

--- app/assets/javascripts/sms.js
+++ app/assets/javascripts/sms.js
@@ -36,30 +36,6 @@ $("#send_sms").on("click",function(){
   });
 })
 
-
-  // $("#sms_verify").on 'click', ->
-  //   $("#new_by_mobile_div").showLoading();
-  //   param = $('#sms_register_from').serialize()
-  //   url = '/users/sms_verify'
-  //   $.ajax
-  //     url: url,
-  //     async:false,
-  //     method: 'post',
-  //     dataType: 'json',
-  //     data: param,
-  //     success: (data) ->
-  //       console.info(data)
-  //       result = data['SubmitResult']
-  //       if result['code'] == '2'
-  //         location.href = "/users/invitation/accept?invitation_token="+result['invitation_token']
-  //         #$("#verify_invitation_token").val result['invitation_token']
-  //       else
-  //         showNotice result['msg']
-  //       $("#new_by_mobile_div").hideLoading();
-  //     error: (msg) ->
-  //       $("#new_by_mobile_div").hideLoading();
-  //       showNotice "error"
-
 $("#sms_verify").on("click",function(){
   params = $("#registration_form").serialize();
 
