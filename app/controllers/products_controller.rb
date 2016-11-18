850596d25fdffe9affed5f609a23f78f649d9f66
chenxi <chenxi@tigerjoys.com>
2016-11-18 20:02:22 +0800

    fixed product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
+    # @products = search
     @products = Product.all
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,12 @@
 class Product < ActiveRecord::Base
 	belongs_to :shop
 	belongs_to :order
+
+	PRODUCT_ALL = "product_all"
+	# def search
+	#     products = Rails.cache.exist?(PRODUCT_ALL) ? Rails.cache.read(PRODUCT_ALL) : Product.all
+	#     Rails.cache.write(cache_key, products)
+	#     products
+	# end
+
 end
