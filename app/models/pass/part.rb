f1ac748a3ca0ceb5bf27ccc23390165125f1117f
yuzhiyuan413 <1048921160@qq.com>
2016-11-18 17:41:21 +0800

    function.rb

--- app/models/pass/part.rb
+++ app/models/pass/part.rb
@@ -1,18 +1,18 @@
 # encoding: utf-8
 class Pass::Part
-	attr_accessor :id, :name, :controller, :action, :url, :hidden, :controllers, :subsystem, :selected, :icon_class
+	attr_accessor :id, :sort, :name, :controller, :action, :url, :hidden, :controllers, :subsystem, :selected, :icon_class
 	def attributes attrs
-		raise ArgumentError, "Part的id不能为空" if attr[:id].to_i == 0
-		raise ArgumentError, "part:#{attr[:id]}" if Pass::System.parts.any? { |s| s.id.to_i == attr[:id].to_i }
+		raise ArgumentError, "Part的id不能为空" if attrs[:id].to_i == 0
+		raise ArgumentError, "part:#{attrs[:id]}" if Pass::System.parts.any? { |s| s.id.to_i == attrs[:id].to_i }
 		attrs.each do |k,v| 
 			self.send("#{k}=",v) if v.present?
 		end
 	end
 
-	def define_controller attr
+	def define_controller attr, &block
 		c = Pass::Controller.new attr
     c.part = self
     Pass::System.controllers << c
-    instance_eval(c)
+    c.instance_eval(&block) if block_given?
 	end
 end
--- app/models/pass/subsystem.rb
+++ app/models/pass/subsystem.rb
@@ -2,19 +2,19 @@
 class Pass::Subsystem
 	attr_accessor :id, :name, :parts, :icon_class, :controller, :action, :sort, :hidden
 	def attributes attrs
-		raise ArgumentError, "已存在相同subsystem:#{attr[:id]}" if Pass::System.subsystems.any? { |s| s.id.to_i == attr[:id].to_i }
+		raise ArgumentError, "已存在相同subsystem:#{attrs[:id]}" if Pass::System.subsystems.any? { |s| s.id.to_i == attrs[:id].to_i }
 		attrs.each do |k,v| 
 			self.send("#{k}=",v) if v.present?
 		end
 	end
 
-	def define_part name
+	def define_part name, &block
 		p = Pass::Part.new
 		p.name = name
 		p.subsystem = self
-		parts << p
-		instance_eval(p)
+		Pass::System.parts << p
+		p.instance_eval(&block) if block_given?
 	end
 
-	
+
 end
